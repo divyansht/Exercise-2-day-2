ff7345ba1b36276fd03df5e3b84da9134d8cfef1
div15 <div15@DESKTOP-9JQH6SQ>
2020-01-21 21:18:41 -0500

    Add project files.

---
+++ Exercise 2 day 2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercise_2_day_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int age, height, heartrate; 
+            age = 27;
+            height = 170;
+            heartrate = 220 - age;
+
+            Console.WriteLine("My age is " +age+ " , my max heart rate is " +heartrate+ " and my height is " +height+ " .");
+        }
+    }
+}
